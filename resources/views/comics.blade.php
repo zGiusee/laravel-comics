a1331e1b774e5777d9fee3069a336ba278904404
zGiusee <giusebelforte@gmail.com>
2024-02-13 14:01:30 +0100

    [UPDATE] Created layout

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.base')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-
-    <main>
+@section('content')
+    <div>
         <div class="jumbo-container">
             <span class="default-badge-bg">CURRENT SERIES</span>
         </div>
@@ -50,13 +33,4 @@
                 </div>
             </div>
         </div>
-
-        <div class="my-bg-blue">
-            @include ('partials.infoBanner')
-        </div>
-    </main>
-
-    @include('partials.footer')
-</body>
-
-</html>
+    @endsection
