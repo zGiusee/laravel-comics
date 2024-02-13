1b50f25384af23da8e78877ce6a3ef357925e207
zGiusee <giusebelforte@gmail.com>
2024-02-13 09:55:45 +0100

    [UPDATE] added 'HOME' in header links

--- config/header_links.php
+++ config/header_links.php
@@ -40,4 +40,8 @@ return [
         'name' => 'SHOP',
         'url' => 'shop',
     ],
+    [
+        'name' => 'HOME',
+        'url' => '/',
+    ],
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,12 @@
             <span class="default-badge-bg">CURRENT SERIES</span>
         </div>
 
+        <div class="container py-5">
+            <div class="col-12 py-5">
+                <h1>Qua non ci sta nulla ancora...</h1>
+            </div>
+        </div>
+
         <div class="my-bg-blue">
             @include ('partials.infoBanner')
         </div>
